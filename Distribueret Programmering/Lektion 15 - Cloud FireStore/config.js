40210f13442495e89175e2417a0401f2a103e76d
saphyron <30288325+saphyron@users.noreply.github.com>
2021-04-27 16:40:38 +0200

    quizgame

--- Distribueret Programmering/Lektion 15 - Cloud FireStore/config.js
+++ Distribueret Programmering/Lektion 15 - Cloud FireStore/config.js
@@ -1,1 +1,47 @@
+React:
 
+Ejne metoder
+didmount lifecycle hook
+state mangement
+
+helidon:
+
+billede over arktektur
+Domain
+	2 overordnet klasser: (Question, Answer)
+		Answer: (Id, AnswerName, Explanation, TrueAnswer, Url)
+		Question: (Id, Category(Enum), Field, Description, Points)		
+Filter
+	Corsfilter
+Repository
+	Entity
+		AnsweredQuestionPO
+		AnswerPO
+		QuestionPO
+	EntityManager
+		DemoEntityManager
+		EntityManagerProducer
+		JpaStarter
+	Interfaces
+		IRepository
+	AnswerRepository
+	Mapper
+	QuestionRepository
+Resource
+	dto
+		AnswerDTO
+		CreateAnswerDTO
+		CreateAnsweredDTO
+		CreateQuestionDTO
+		ReadAnswerDTO
+		ReadQuestionDTO
+	AnswerResource
+	Mapper
+	QuestionResource
+Service
+	Request
+		AnsweredRequest
+	AnswerService
+	QuestionService
+DemoApplication
+Main
